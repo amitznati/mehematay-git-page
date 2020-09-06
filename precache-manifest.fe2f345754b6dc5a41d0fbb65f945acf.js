2ba3ab361d6a38676c5813684c553a784612d449
amitznati-work-hp <amitznati@gmail.com>
2020-09-06 23:09:29 +0300

    Initial commit

---
+++ precache-manifest.fe2f345754b6dc5a41d0fbb65f945acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37dfe09370c9ea6712bf585bcb2249dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ae57a9b9293d1d1303b",
+    "url": "./static/js/2.b13f294c.chunk.js"
+  },
+  {
+    "revision": "6acd56cd61d2a383ebc1fb3b7f2e5590",
+    "url": "./static/js/2.b13f294c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969c940e2a38b07f770e",
+    "url": "./static/js/main.972be796.chunk.js"
+  },
+  {
+    "revision": "02c6797f2a916a6fcfb4",
+    "url": "./static/js/runtime-main.b8444c16.js"
+  },
+  {
+    "revision": "b677c0ea5d9bfc7fffed086181e74672",
+    "url": "./static/media/ic_launcher_round.b677c0ea.png"
+  }
+]);
